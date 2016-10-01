a95e655c2ac3a05aed4bc580ee78b40a552735ae
DennisDaddy <softteez95@gmail.com>
2016-10-01 13:20:58 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'pages/about'
+  get 'help' => 'pages#help'
 
-  get 'pages/contact'
+ get 'contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
