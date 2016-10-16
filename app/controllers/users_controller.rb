dd3e9800937a08959f85df2d611fed5698448120
DennisDaddy <softteez95@gmail.com>
2016-10-16 11:20:54 +0300

    Adding a logged_in_user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,5 +47,16 @@ class UsersController < ApplicationController
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	
   end
+
+  #confirms a logged-in user
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+      
+    end
+    
+  end
   
 end
