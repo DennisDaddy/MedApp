619d1cd0ec86bf1fc17c19bbafb4c39243bb802d
DennisDaddy <softteez95@gmail.com>
2016-10-27 12:15:10 +0300

    add a working destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+   redirect_to users_url 
   end
   
   private
